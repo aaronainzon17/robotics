44e77a7952dcec8da8e44200a6c10298d11fa67b
aaronainzon17 <aaronainzon@gmail.com>
2022-03-08 10:29:59 +0100

    asa

--- practica2/Robot.py
+++ practica2/Robot.py
@@ -83,8 +83,7 @@ class Robot:
         print("setting speed to %.2f %.2f" % (v, w))
 
         # compute the speed that should be set in each motor ...
-        im0 = np.array([1/self.R.value, self.L.value/2*self.R.value],
-                                 [1/self.R.value, -self.L.value/2*self.R.value])
+        im0 = np.array([1/self.R.value, self.L.value/2*self.R.value],[1/self.R.value, (-self.L.value)/(2*self.R.value)])
         im1 = np.array([v,w])
         inverse_model = np.dot(im0,im1)
         wd = inverse_model[0]  
@@ -153,7 +152,7 @@ class Robot:
 
             [realv,realw] = self.readSpeed()
                       
-            realth = self.th + realw * self.P
+            realth = self.th.value + realw * self.P
             if realw == 0: 
                 d_x = self.P.value * realv * np.cos(realth) # Duda de si es self.th.value 
                 d_y = self.P.value *  realv * np.sin(realth)
