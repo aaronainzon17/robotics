4f06ddd73aed69b45152e532bbe391a18d168f9b
Gancedo <59729678+Gancedo@users.noreply.github.com>
2022-03-10 13:00:51 +0100

    prueba 1

--- practica2/p2_base.py
+++ practica2/p2_base.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import argparse
+from tokenize import Double
 import numpy as np
 import time
 from Robot import Robot
+
+
+def dos_puntos(robot, a, d, r2):
+    a
+
+
+def ocho(robot, d):
+    v = 200
+    w = np.rad2deg((Double)(200/d))
+
+    robot.setSpeed(0, -90)
+    time.sleep(1)
+    robot.setSpeed(v, w)
+    time.sleep(180/w)
+    robot.setSpeed(v, -w)
+    time.sleep(360/w)
+    robot.setSpeed(v, w)
+    time.sleep(180/w)
+    robot.setSpeed(0, 0)
 
 
 def main(args):
@@ -16,50 +36,54 @@ def main(args):
         # robot = Robot(init_position=args.pos_ini)
         robot = Robot()
 
-        print("X value at the beginning from main X= %.2f" %(robot.x.value))
+        print("X value at the beginning from main X= %.2f" % (robot.x.value))
 
         # 1. launch updateOdometry Process()
         robot.startOdometry()
 
         # 2. perform trajectory
 
-        #RECTANGLE
+        # RECTANGLE
         print("Start : %s" % time.ctime())
-        
-        robot.setSpeed(200,0)
-        time.sleep(4)
-        robot.setSpeed(0,90)
-        time.sleep(1)
-        robot.setSpeed(200,0)
-        time.sleep(2)
-        robot.setSpeed(0,90)
-        time.sleep(1)
-        robot.setSpeed(200,0)
-        time.sleep(4)
-        robot.setSpeed(0,90)
-        time.sleep(1)
-        robot.setSpeed(200,0)
-        time.sleep(2)
-        robot.setSpeed(0,90)
-        time.sleep(1)
+
+        # robot.setSpeed(200, 0)
+        # time.sleep(4)
+        # robot.setSpeed(0, 90)
+        # time.sleep(1)
+        # robot.setSpeed(200, 0)
+        # time.sleep(2)
+        # robot.setSpeed(0, 90)
+        # time.sleep(1)
+        # robot.setSpeed(200, 0)
+        # time.sleep(4)
+        # robot.setSpeed(0, 90)
+        # time.sleep(1)
+        # robot.setSpeed(200, 0)
+        # time.sleep(2)
+        # robot.setSpeed(0, 90)
+        # time.sleep(1)
+
+        robot.setSpeed(200, 0)
+        time.sleep(6)
 
         print("End : %s" % time.ctime())
-        
+
         robot.lock_odometry.acquire()
-        print("Odom values at main at the END: %.2f, %.2f, %.2f " % (robot.x.value, robot.y.value, robot.th.value))
+        print("Odom values at main at the END: %.2f, %.2f, %.2f " %
+              (robot.x.value, robot.y.value, robot.th.value))
         robot.lock_odometry.release()
-        
+
         # DUMMY CODE! delete when you have your own
-        #robot.setSpeed(0.25,0)
+        # robot.setSpeed(0.25,0)
         #print("Start : %s" % time.ctime())
-        #time.sleep(3)
+        # time.sleep(3)
         #print("X value from main tmp %d" % robot.x.value)
-        #time.sleep(3)
+        # time.sleep(3)
         #print("End : %s" % time.ctime())
 
-        #robot.lock_odometry.acquire()
+        # robot.lock_odometry.acquire()
         #print("Odom values at main at the END: %.2f, %.2f, %.2f " % (robot.x.value, robot.y.value, robot.th.value))
-        #robot.lock_odometry.release()
+        # robot.lock_odometry.release()
 
         # PART 1:
         # robot.setSpeed()
@@ -71,18 +95,16 @@ def main(args):
 
         # ...
 
-
-
         # 3. wrap up and close stuff ...
         # This currently unconfigure the sensors, disable the motors,
         # and restore the LED to the control of the BrickPi3 firmware.
         robot.stopOdometry()
 
-
     except KeyboardInterrupt:
-    # except the program gets interrupted by Ctrl+C on the keyboard.
-    # THIS IS IMPORTANT if we want that motors STOP when we Ctrl+C ...
+        # except the program gets interrupted by Ctrl+C on the keyboard.
+        # THIS IS IMPORTANT if we want that motors STOP when we Ctrl+C ...
         robot.stopOdometry()
+
 
 if __name__ == "__main__":
 
@@ -94,6 +116,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args)
-
-
-
