630230fa5b81b64f8eaa47ccec4a5ee43e04da28
aaronainzon17 <aaronainzon@gmail.com>
2022-03-21 17:08:33 +0100

    a

--- practica2/p2_base.py
+++ practica2/p2_base.py
@@ -156,7 +156,7 @@ def ocho(robot, d):
 
     robot.setSpeed(v, w)
     check_position(robot, 0, 2*d, normalizar(np.deg2rad(90)),
-                   25, 25, np.deg2rad(3))
+                   50, 50, np.deg2rad(5))
 
     robot.setSpeed(v, -w)
     check_position(robot, 0, 2*d, normalizar(np.deg2rad(90)),
