a16d3e728c9200ab57c0f52583827ff4a202e086
aaronainzon17 <aaronainzon@gmail.com>
2022-03-21 17:31:10 +0100

    a

--- practica2/p2_base.py
+++ practica2/p2_base.py
@@ -106,7 +106,7 @@ def check_position(robot, x, y, th, x_err, y_err, angular_err):
         #print("ERROR: no ha parado y se ha superado el umbral")
         # robot.setSpeed(0,0)
 
-        if (abs(abs(x)-abs(x_now)) <= x_err) and (abs(abs(y)-abs(y_now)) <= y_err) and (abs(abs(th)-abs(th_now)) <= angular_err):
+        if (abs(x-x_now) <= x_err) and (abs(y-y_now) <= y_err) and (abs(th-th_now) <= angular_err):
             # if (abs(x-x_now) <= pos_err) or (abs(y-y_now) <= pos_err) or (abs(th-th_now) <= angular_err):
             reached = True
             print("Se ha alcanzado el punto:[",
@@ -160,7 +160,7 @@ def ocho(robot, d):
 
     robot.setSpeed(0, -45)
     check_position(robot, 0, 0, normalizar(np.deg2rad(-90)),
-                   np.Infinity, np.Infinity, np.deg2rad(1))
+                   np.Infinity, np.Infinity, np.deg2rad(2))
 
     robot.setSpeed(v, w)
     check_position(robot, 2*d, 0, normalizar(np.deg2rad(90)),
