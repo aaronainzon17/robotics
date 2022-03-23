2ff694871dffc8459ce7f515fbecad8caf029644
aaronainzon17 <aaronainzon@gmail.com>
2022-03-23 19:42:21 +0100

    EJ2 de locos

--- Ejercicio2/Ej2.py
+++ Ejercicio2/Ej2.py
@@ -2,10 +2,6 @@ import numpy as np
 import math 
 import matplotlib.pyplot as plt
 
-pi = 3.141592653589793
-tau = 6.283185307179586476925287
-
-
 # Dibuja robot en location_eje con color (c) y tamano (p/g)
 def dibrobot(loc_eje,c,tamano):
   if tamano=='p':
@@ -33,14 +29,14 @@ def dibrobot(loc_eje,c,tamano):
   robot=np.dot(Hwe,np.dot(Hec,np.transpose(extremos)))
   
   plt.plot(robot[0,:], robot[1,:], c)
-  #plt.plot(0,10)
-  #plt.plot(0,0)
-  #plt.plot(5,0)
-  
-  #plt.show()
 
-def norm_pi(num):
-    return (num + pi)%tau - pi
+def norm_pi(th):
+        """ Funcion de normalizacion del angulo entre -pi, pi """
+        if th > math.pi:
+            th = th - 2 * math.pi
+        elif th < -math.pi:
+            th = th + 2 * math.pi
+        return th
 
 def simubot(vc,xWR,T):
   if vc[1]==0:   # w=0
@@ -52,45 +48,32 @@ def simubot(vc,xWR,T):
       xRk=np.array([R*np.sin(titak), R*(1-np.cos(titak)), titak])  
 
   xWRp=loc(np.dot(hom(xWR),hom(xRk)))   # nueva localizaci�n xWR
+
   return xWRp
 
-#hom(X) dada una lista de [x,y,tita] se obtiene la matriz homogenea
-# X debe ser una lista de la forma [x,y,tita]
-def hom(X):
+def hom(x):
+    x1 = x[0]; y1 = x[1]; th = x[2]
 
-    #cos y sen funcionan con radianes por eso se transforman los grados en radianes
-    matrizHomogenea = np.array([[math.cos(X[2]) ,-math.sin(X[2]),X[0]],[math.sin(X[2]) ,math.cos(X[2]),X[1]],[0,0,1]])
-    return matrizHomogenea
+    T = np.array([
+        [np.cos(th), - np.sin(th), x1],
+        [np.sin(th), np.cos(th), y1],
+        [0, 0, 1]])
 
-#La funcion recibe una matriz y devuelve la lista [x,y,tita]
+    return T
+
 def loc(T):
-    #coor=np.array([T[0][2],T[1][2],math.degrees(math.atan2(T[1][0],T[0][0]))])
-    coor=np.array([T[0][2],T[1][2],math.atan2(T[1][0],T[0][0])])
-    return coor
+    nx = T[0][0]; ny = T[1][0]
+    px = T[0][2]; py = T[1][2]
+    x = np.array([px, py,norm_pi(np.arctan2(ny,nx))])
 
+    return x
 
-
-"""
-do
-    gxr = loc(hom(gxw)*hom(wxr))
-    gpr_plus = polares(gxr) #polares sera una funcion de python o hecha por nosotros
-    [vr,wr] = K * gpr_plus
-    wxr_plus = simutobot(wxr,vr,wr)
-    wxr = wxr_plus
-while   !ultimo_objetivo
-"""
-
-#Si te dan como se ve algo desde el mundo, te devuelve como se ve el mundo desde algo
-def invertir(wxg):
-    gwx = wxg * -1
-    return gwx
-
-#def cart2pol(x, y, th):
 def cart2pol(x, y, th):
-    rho = np.sqrt(x**2 + y**2)
-    phi = np.arctan2(y, x)
-    alpha = phi - th
-    return np.array([rho, phi,alpha])
+    rho = np.sqrt(np.power(x,2) + np.power(y,2))
+    beta = norm_pi(np.arctan2(y, x) + math.pi)
+    
+    alpha = beta - th
+    return np.array([[rho], [alpha], [beta]])
 
 def pol2cart(rho, phi):
     x = rho * np.cos(phi)
@@ -101,28 +84,41 @@ def Ej2():
     indexObjectivoActual = 0
     recorrido = np.array([[2,3,np.deg2rad(90)],[6,4,np.deg2rad(45)],[10,5,np.deg2rad(-45)],[7,-3,np.deg2rad(180)],[2,3,np.deg2rad(90)]])
     objetivoActual = recorrido[0]
-    wxr = np.array([0,0,0]) #esta es la inicial
-    gxw = invertir(objetivoActual)
+    wxr = np.array([0,0,np.deg2rad(0)]) # Posicion inical del robot con respecto al mundo
+    
     objetivoAlcanzado = False
-    K = np.array([[0.8,0,0],[0,0.9,0.8]])
+    k = np.array([[0.35,0,0],[0,0.45,0.4]])
     
     i = 0
     while not objetivoAlcanzado:
-        gxr = loc(hom(gxw)*hom(wxr))
+        
+        gTr = np.dot(np.linalg.inv(hom(objetivoActual)),hom(wxr))
+        gxr = loc(gTr)
+        
         gpr_plus = cart2pol(gxr[0],gxr[1],gxr[2])
-        V = K * gpr_plus
-        vr = V[0][0]
-        wr = V[1][0]
+        
+        vel = np.dot(k,gpr_plus)
+        vr = vel[0][0]
+        wr = vel[1][0]
+
+        if vr > 3 or wr > 3: 
+            print(vr , wr)
+            input()
+        
         wxr_plus = simubot(np.array([vr,wr]),wxr,i)
         wxr = wxr_plus
-        print("Pisicion actual del robot en el mundo",wxr)
-        if np.array_equal(wxr, objetivoActual):
-            dibrobot(wxr,'c','p')
-            indexObjectivoActual+=1
-            objetivoActual = recorrido[indexObjectivoActual]
+        dibrobot(wxr,'c','p')
+        
+        if gxr[0] < 0.05 and gxr[0] > -0.05 and gxr[1] < 0.05 and gxr[1] > -0.05 and gxr[2] < 0.05 and gxr[2] > -0.05:
+        
+            indexObjectivoActual += 1
             if(indexObjectivoActual == 5):  #Ha acabado la trayectoria
                 objetivoAlcanzado = True
-        i += 0.2
+            else:
+                print('Trayectoria', indexObjectivoActual)
+                objetivoActual = recorrido[indexObjectivoActual]
+        i += 0.00001
+        
     plt.show()
 
 
