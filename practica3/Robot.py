7f6c3c088aaea66f58b4844bbe44190d81ad876b
aaronainzon17 <aaronainzon@gmail.com>
2022-03-31 15:37:43 +0200

    Co-authored-by: SergioGabete <SergioGabete@users.noreply.github.com>

--- practica3/Robot.py
+++ practica3/Robot.py
@@ -252,7 +252,8 @@ class Robot:
 
         # Inicializar la camara del robot 
         cam = cv2.VideoCapture(0)
-       
+        cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+        cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
         # allow the camera to warmup
         time.sleep(0.1)
         
