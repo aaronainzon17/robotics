e8d23e18fac9917beb4c5567d60a8b4d1b0d8768
SergioGabete <774631@unizar.es>
2022-04-01 15:32:33 +0200

    fff

--- practica3/Robot.py
+++ practica3/Robot.py
@@ -273,7 +273,7 @@ class Robot:
             if (blob is not None and almost_centered):
                 x_actual = blob.pt[0]
                 print("X_Blob = ", blob.pt[0], ", Y_Blob = ", blob.pt[1],", Blob_Size= ", blob.size)
-                if blob.size > 125:
+                if blob.size > 110:
                     print('Paro porque he encontrado un blob de',blob.size)
                     finished = True
                     
