ce63a56810277f6d6db6e139dff8f4c75d1c82c2
aaronainzon17 <aaronainzon@gmail.com>
2022-04-01 18:16:48 +0200

    a

--- practica3/Robot.py
+++ practica3/Robot.py
@@ -309,16 +309,14 @@ class Robot:
                 x_bl,y_bl = [blob.pt[0],blob.pt[1]]
                 print(x_bl,y_bl)
                 print("X_Blob = ", blob.pt[0], ", Y_Blob = ", blob.pt[1],", Blob_Size= ", blob.size)
-                cv2.imshow('Final img', imgBGR)
-                cv2.waitKey(0)
                 if (y_bl > rows/8) and (abs(x_bl - cols/2) < 50):
                     finished = True
-                    cv2.imshow('Final img', imgBGR)
-                    cv2.waitKey(0)
+                    #cv2.imshow('Final img', imgBGR)
+                    #cv2.waitKey(0)
                 else:
                     print('No se ve la pelota en las pinzas')
-                    cv2.imshow('Final img', imgBGR)
-                    cv2.waitKey(0)
+                    #cv2.imshow('Final img', imgBGR)
+                    #cv2.waitKey(0)
                     triedCatch = False
                    
         return finished
