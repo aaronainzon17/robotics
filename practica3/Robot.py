6108f672e03e68501ba6711cf1eb20e16d2876b7
aaronainzon17 <aaronainzon@gmail.com>
2022-04-04 12:18:25 +0200

    prueba

--- practica3/Robot.py
+++ practica3/Robot.py
@@ -277,17 +277,17 @@ class Robot:
                   
             else:
                 mid_img = cols/2 # Se calcula en eje central de la imagen
-                
+
                 # Si se ha encontrado la pelota en la imagen se ralentiza el giro hasta centrarla
                 if (blob is not None):  
                     last_bloob = blob
-                    self.find_ball(last_bloob, mid_img, 20) # Se ralentiza el giro PREV: self.setSpeed(0,-20)
+                    self.find_ball(last_bloob, 20, mid_img) # Se ralentiza el giro PREV: self.setSpeed(0,-20)
                     # Si se encuentra a 80 pixeles del centro 
                     if abs(blob.pt[0] - mid_img) < 80:
                         almost_centered = True # Se indica que el blob esta casi centrado
                 else:
                     # Si no se ha encontrado la pelota en la imagen se comienza a girar para buscar la pelota
-                    self.find_ball(last_bloob, mid_img, 40) # PREV = self.setSpeed(0,-40)
+                    self.find_ball(last_bloob, 40, mid_img) # PREV = self.setSpeed(0,-40)
                     almost_centered = False
 
             # Si previamente se ha realizado un intento de coger se comprueba si la pelota esta en las pinzas
