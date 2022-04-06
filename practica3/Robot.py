ff058936bfc24c984b42e2cbd5dcf724e42d8a12
aaronainzon17 <aaronainzon@gmail.com>
2022-04-06 16:35:55 +0200

    as

--- practica3/Robot.py
+++ practica3/Robot.py
@@ -272,7 +272,7 @@ class Robot:
                     self.trackObjectSpeed(x_actual,self.cols.value)  
             else:
                 # En caso en el que la pelota desaparezca de la imagen se inicia busqueda
-                print('Pierdo el blob')
+                #print('Pierdo el blob')
                 self.find_ball(80)
                 
             # Si previamente se ha realizado un intento de coger se comprueba si la pelota esta en las pinzas
