e879961f421182c7553c9f7cc28c82f5aef6644d
aaronainzon17 <aaronainzon@gmail.com>
2022-04-21 11:23:39 +0200

    as

--- practica4/p4_main.py
+++ practica4/p4_main.py
@@ -82,6 +82,6 @@ if __name__ == "__main__":
     # Add as many args as you need ...
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--mapfile", help="path to find map file",
-                        default="mapa1.txt")
+                        default="mapa0.txt")
     args = parser.parse_args()
     main(args)
