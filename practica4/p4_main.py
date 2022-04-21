d494878c6fac38f83aaf736edb04289fac8a6ff4
aaronainzon17 <aaronainzon@gmail.com>
2022-04-21 09:40:20 +0200

    prueba 1

--- practica4/p4_main.py
+++ practica4/p4_main.py
@@ -44,7 +44,7 @@ def main(args):
         myMap = Map2D(map_file)
         myMap.fillCostMatrix(2, 2, True)
         #myMap.verbose = True
-        myMap.drawMap(saveSnapshot=False)
+        #myMap.drawMap(saveSnapshot=False)
         myMap.findPath([0,0],[2,2], True)
         #myMap.planPath(0, 0, 6, 0, True)
         print(myMap.currentPath)
