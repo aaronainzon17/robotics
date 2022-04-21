611928880bb22e0d354ecdcf051a4dd299c5d96b
aaronainzon17 <aaronainzon@gmail.com>
2022-04-21 09:38:47 +0200

    w

--- practica4/Robot.py
+++ practica4/Robot.py
@@ -19,8 +19,6 @@ from picamera.array import PiRGBArray
 # tambien se podria utilizar el paquete de threading
 from multiprocessing import Process, Value, Array, Lock
 
-from get_color_blobs import getRedBloobs
-
 
 class Robot:
     def __init__(self, init_position=[0.0, 0.0, 0.0]):
