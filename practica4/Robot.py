0d89cd1d0fd1e0c119eb89b52b845526ab777025
aaronainzon17 <aaronainzon@gmail.com>
2022-04-22 19:20:03 +0200

    eq

--- practica4/Robot.py
+++ practica4/Robot.py
@@ -477,7 +477,8 @@ class Robot:
         return th
 
     def detectObstacle(self, x_goal, y_goal):
-        while True and value <= 0.0:
+        value = 0.0
+        while value <= 0.0:
             try:
                 [x_now, y_now, _] = self.readOdometry()
                 espacio = np.linalg.norm([x_goal - x_now, y_goal - y_now])
