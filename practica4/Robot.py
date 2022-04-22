53dd39ce790df362c9754964c3e8344478360082
aaronainzon17 <aaronainzon@gmail.com>
2022-04-22 19:17:51 +0200

    hg

--- practica4/Robot.py
+++ practica4/Robot.py
@@ -477,18 +477,19 @@ class Robot:
         return th
 
     def detectObstacle(self, x_goal, y_goal):
-        try:
-            [x_now, y_now, _] = self.readOdometry()
-            espacio = np.linalg.norm([x_goal - x_now, y_goal - y_now])
-            print('La doistancia es:', espacio)
-            value = self.BP.get_sensor(self.BP.PORT_1) * 10
-            print('He leido: ', value)
-            if value < espacio:
-                return True
-            else:
-                return False
-        except brickpi3.SensorError as error:
-            print(error) 
+        while True:
+            try:
+                [x_now, y_now, _] = self.readOdometry()
+                espacio = np.linalg.norm([x_goal - x_now, y_goal - y_now])
+                print('La doistancia es:', espacio)
+                value = self.BP.get_sensor(self.BP.PORT_1) * 10
+                print('He leido: ', value)
+                if value < espacio:
+                    return True
+                else:
+                    return False
+            except brickpi3.SensorError as error:
+                print(error) 
 
 
 #########################################################
