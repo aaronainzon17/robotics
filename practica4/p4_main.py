1a4c0f2563b7ba147cef1cbea34f607dc6bbaa9b
aaronainzon17 <aaronainzon@gmail.com>
2022-05-10 17:01:32 +0200

    cambios p4

--- practica4/p4_main.py
+++ practica4/p4_main.py
@@ -21,7 +21,58 @@ matplotlib.use("TkAgg")
 def solveMap(robot, map_file, point_ini, point_end, ocho=False):
     # 1. load map and compute costs and path
     myMap = Map2D(map_file)
-    myMap.fillCostMatrix(3, 3, True)
+    
+    # Se calcula el camino inicial
+    if not myMap.findPath(point_ini,point_end, ocho):
+        # En caso de que no exista en camino a la celda indicada
+        print('NO EXISTE CAMINO DISPONIBLE')
+        robot.setSpeed(0,45)
+        time.sleep(5)
+        robot.setSpeed(0,0)
+        exit(1)
+    
+    path2print = []
+    goal_reached = False
+    lost = False
+    # Se intenta resolver el camino hasta que se consiga o no haya camino disponible 
+    # por los obstaculos no registrados 
+    while not goal_reached and not lost:
+
+        prev_point = np.array(point_ini)
+        for point_map in myMap.currentPath:
+            point = [200+point_map[0]*400, 200+point_map[1]*400, 1.57]
+            
+            # Si de detecta obstaculo 
+            if robot.detectObstacle(point[0],point[1]):
+                
+                robot.setSpeed(0,0)
+                [_,_,th] = robot.readOdometry()
+                # se anyade el obsatculo al mapa 
+                ocho = myMap.setNewObstacle(prev_point, th, ocho)
+                
+                # Se recalcula el camino con el nuevo obstaculo 
+                if not myMap.replanPath(prev_point[0],prev_point[1],point_end[0], point_end[1],ocho):
+                    # En caso de que no exista en camino a la celda indicada
+                    print('NO EXISTE CAMINO DISPONIBLE CON EL NUEVO OBSTACULO DETECTADO')
+                    robot.setSpeed(0,45)
+                    time.sleep(5)
+                    robot.setSpeed(0,0)
+                    # Se indica que el robot se ha "perdido"
+                    lost = True
+                
+                goal_reached = False
+                break
+            else:
+                goal_reached = True
+
+            # Se mueve el robot a la siguiente celda
+            robot.go(point[0],point[1])
+            path2print.append([point[0], point[1], 1.57])
+            prev_point = point_map
+    
+    # Al acabar se muestra el recorrido realizado
+    myMap.drawMapWithRobotLocations(
+        path2print, saveSnapshot=False)
 
 def main(args):
     
@@ -45,59 +96,7 @@ def main(args):
         # Se inicia la odometria
         robot.startOdometry()
 
-        # 1. load map and compute costs and path
-        myMap = Map2D(map_file)
-        
-        # Se calcula el camino inicial
-        if not myMap.findPath(ini,goal, ocho):
-            # En caso de que no exista en camino a la celda indicada
-            print('NO EXISTE CAMINO DISPONIBLE')
-            robot.setSpeed(0,45)
-            time.sleep(5)
-            robot.setSpeed(0,0)
-            exit(1)
-        
-        path2print = []
-        goal_reached = False
-        lost = False
-        # Se intenta resolver el camino hasta que se consiga o no haya camino disponible 
-        # por los obstaculos no registrados 
-        while not goal_reached and not lost:
-
-            prev_point = np.array(ini)
-            for point_map in myMap.currentPath:
-                point = [200+point_map[0]*400, 200+point_map[1]*400, 1.57]
-                # Si de detecta obstaculo 
-                if robot.detectObstacle(point[0],point[1]):
-                    
-                    robot.setSpeed(0,0)
-                    [_,_,th] = robot.readOdometry()
-                    # se anyade el obsatculo al mapa 
-                    ocho = myMap.setNewObstacle(prev_point, th, ocho)
-                    
-                    # Se recalcula el camino con el nuevo obstaculo 
-                    if not myMap.replanPath(prev_point[0],prev_point[1],goal[0], goal[1],ocho):
-                        # En caso de que no exista en camino a la celda indicada
-                        print('NO EXISTE CAMINO DISPONIBLE CON EL NUEVO OBSTACULO DETECTADO')
-                        robot.setSpeed(0,45)
-                        time.sleep(5)
-                        robot.setSpeed(0,0)
-                        # Se indica que el robot se ha "perdido"
-                        lost = True
-                    
-                    goal_reached = False
-                    break
-                else:
-                    goal_reached = True
-
-                # Se mueve el robot a la siguiente celda
-                robot.go(point[0],point[1])
-                path2print.append([point[0], point[1], 1.57])
-                prev_point = point_map
-        
-        # Al acabar se muestra el recorrido realizado
-        myMap.drawMapWithRobotLocations(
-            path2print, saveSnapshot=False)
+        solveMap(robot,map_file,ini,goal,ocho)
         
         # Se detiene la odometria
         robot.stopOdometry()
