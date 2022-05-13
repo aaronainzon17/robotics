4d7ee2c372a9665560fe70dd3df05b473783ddbf
SergioGabete <774631@unizar.es>
2022-05-13 12:02:30 +0200

    Update Robot.py

--- libraries/Robot.py
+++ libraries/Robot.py
@@ -598,12 +598,9 @@ class Robot:
         time.sleep(1)
         self.setSpeed(0,30)
         while(not self.is_blob.value):
-            
-            print("El blob es nulo")
-
+            pass
         while(self.x_b.value<=self.cols.value/2):
-            #self.setSpeed(0,30)
-            print("El blob esta en ",self.x_b.value)
+            pass
         self.setSpeed(0,0)
         
 
