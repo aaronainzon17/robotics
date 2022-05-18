6c331e13a6e03b293de1be4576ff2dc9a83de00f
aaronainzon17 <aaronainzon@gmail.com>
2022-05-18 19:19:50 +0200

    prueba

--- Trabajo/libraries/Robot.py
+++ Trabajo/libraries/Robot.py
@@ -411,8 +411,9 @@ class Robot:
         time.sleep(0.7)
         self.setSpeed(0,0)
         self.BP.set_motor_dps(self.BP.PORT_A, w)
-        time.sleep(0.4) # Bajar cesta
+        time.sleep(0.45) # Bajar cesta
         self.BP.set_motor_dps(self.BP.PORT_A, 0)
+        time.sleep(0.1)
         
 
     def uncatch(self):
