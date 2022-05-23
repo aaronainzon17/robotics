87b8e98e3ad1131c1de692887bd26e6ab7bafdc0
aaronainzon17 <aaronainzon@gmail.com>
2022-05-23 20:22:35 +0200

    a

--- Trabajo/libraries/Robot.py
+++ Trabajo/libraries/Robot.py
@@ -213,10 +213,8 @@ class Robot:
                 d_y = (real_v * self.P) * np.sin(self.th.value)
                 d_th = 0
             else:
-                abs_th = self.norm_pi(np.deg2rad(self.BP.PORT_4[0] *-1)) #self.read_gyros()
-                #print('acabo')
                 # El radio se calcula R = v/w
-                d_th = self.norm_pi(abs_th - self.th.value) #real_w * self.P
+                d_th = real_w * self.P
                 d_s = (real_v/real_w) * d_th
                 d_x = d_s * np.cos(self.th.value + (d_th/2))
                 d_y = d_s * np.sin(self.th.value + (d_th/2))
