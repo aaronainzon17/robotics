81cc052ad81195cc73bec8f3b3882b6250e85efa
aaronainzon17 <aaronainzon@gmail.com>
2022-05-23 16:39:49 +0200

    prueba giros

--- Trabajo/libraries/Robot.py
+++ Trabajo/libraries/Robot.py
@@ -212,8 +212,9 @@ class Robot:
                 d_y = (real_v * self.P) * np.sin(self.th.value)
                 d_th = 0
             else:
+                abs_th = self.norm_pi(np.deg2rad(self.BP.get_sensor(self.BP.PORT_4)[1] * -1))
                 # El radio se calcula R = v/w
-                d_th = real_w * self.P
+                d_th = abs_th * self.P #real_w * self.P
                 d_s = (real_v/real_w) * d_th
                 d_x = d_s * np.cos(self.th.value + (d_th/2))
                 d_y = d_s * np.sin(self.th.value + (d_th/2))
