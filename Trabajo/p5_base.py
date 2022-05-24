b1d6a2e961e96809832cfdb7ddf9eeb9c7ccc08f
aaronainzon17 <aaronainzon@gmail.com>
2022-05-24 19:03:53 +0200

    prueba 60cm

--- Trabajo/p5_base.py
+++ Trabajo/p5_base.py
@@ -148,6 +148,7 @@ def main(args):
         if mapa == "mapaA_CARRERA.txt":
             pos = robot.readOdometry()
             print('Empiezo en', pos)
+            robot.setNewPosition(2200,1800,np.deg2rad(90))
             robot.seguimientoPared(100)
             #mov_debug(robot, 100)
             #s_A(robot, 100)
