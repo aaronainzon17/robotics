873551cb7f1e796adfc8070cee0b179bf13d817f
aaronainzon17 <aaronainzon@gmail.com>
2022-05-27 16:42:28 +0200

    a

--- Trabajo/p5_base.py
+++ Trabajo/p5_base.py
@@ -188,6 +188,7 @@ def main(args):
             #solve_relative_map(robot,myMap, [1,2],[3,3])
             
             #solveMap(robot,myMap, [1,2],[3,3])
+            time.sleep(2)
             robot.setNewPosition(1600,1400,np.deg2rad(90))
             robot.detect_scape()
             # Se inicia la busqueda de la pelota 
