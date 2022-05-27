ae027a6899c07f6f795e6206c7f77d1b016f9117
aaronainzon17 <aaronainzon@gmail.com>
2022-05-27 16:41:43 +0200

    prueba detect salida

--- Trabajo/p5_base.py
+++ Trabajo/p5_base.py
@@ -188,7 +188,7 @@ def main(args):
             #solve_relative_map(robot,myMap, [1,2],[3,3])
             
             #solveMap(robot,myMap, [1,2],[3,3])
-            
+            robot.setNewPosition(1600,1400,np.deg2rad(90))
             robot.detect_scape()
             # Se inicia la busqueda de la pelota 
             #robot.trackObject(colorRangeMin=[0,0,0], colorRangeMax=[255,255,255])
