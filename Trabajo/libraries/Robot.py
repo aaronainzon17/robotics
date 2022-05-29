52a44a7eb885753c3cf05a3db155e78cf86f3496
aaronainzon17 <aaronainzon@gmail.com>
2022-05-29 12:24:14 +0200

    cambio trackball

--- Trabajo/libraries/Robot.py
+++ Trabajo/libraries/Robot.py
@@ -299,7 +299,7 @@ class Robot:
                 x_actual = self.x_b.value # Se obtiene la coordenada x en la que se encuentra
 
                 # Si el diametro es mayor que 150 se inica el proceso de catch porque esta muy cerca del robot
-                if self.size_b.value > 220 and not triedCatch and abs(self.x_b.value - self.cols.value/2) < 50:
+                if self.size_b.value > 210 and not triedCatch and abs(self.x_b.value - self.cols.value/2) < 50:
                     targetPositionReached = True # Se indica que se ha alcanzado el objeto 
                     self.setSpeed(0,0)
                 else:
