741dc19951af8cc2b5fc1b1191095f34e9a00981
aaronainzon17 <aaronainzon@gmail.com>
2022-05-29 09:56:51 +0200

    prueba s_A

--- Trabajo/debug_file.py
+++ Trabajo/debug_file.py
@@ -172,8 +172,9 @@ def main(args):
         robot.startOdometry() 
 
         #half_semicircle(robot)
-        straight_line(robot)
+        #straight_line(robot)
         #rotate_360(robot)
+        s_A_ocho(robot,120)
 
         robot.stopOdometry()
 
