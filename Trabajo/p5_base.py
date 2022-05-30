d92a5320f863f9b76eeed82faa3ccf3bf11ef56c
aaronainzon17 <aaronainzon@gmail.com>
2022-05-30 09:22:29 +0200

    as

--- Trabajo/p5_base.py
+++ Trabajo/p5_base.py
@@ -190,17 +190,17 @@ def main(args):
             # print('Empiezo en', pos)
             # robot.setNewPosition(0,0,np.deg2rad(-180))
             
-            #s_A_ocho(robot, 120)
-            #solve_relative_map(robot,myMap, [1,2],[3,3])
+            s_A_ocho(robot, 120)
+            solve_relative_map(robot,myMap, [1,2],[3,3])
             #print('Hago solveMap')
             #solveMap(robot,myMap, [1,2],[3,3])
             #robot.setNewPosition(1600,1400,np.deg2rad(90))
-            # robot.detect_scape_cv2()
+        
             # # Se inicia la busqueda de la pelota 
             robot.trackObject(colorRangeMin=[0,0,0], colorRangeMax=[255,255,255])
-
+            #robot.detect_scape_cv2()
             #Sale desplues de coger la pelota
-            #robot.scape()
+            robot.scape()
             
         else: # "mapaB_CARRERA.txt"
             robot.trackObject(colorRangeMin=[0,0,0], colorRangeMax=[255,255,255])
