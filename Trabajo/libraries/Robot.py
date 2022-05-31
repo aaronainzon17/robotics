c76c5c2fcbba81c58436718d4c37c3ee7aaa8b2a
aaronainzon17 <aaronainzon@gmail.com>
2022-05-31 15:28:22 +0200

    cambio error a 10

--- Trabajo/libraries/Robot.py
+++ Trabajo/libraries/Robot.py
@@ -731,7 +731,7 @@ class Robot:
         #self.go(center_table[0],center_table[1],150)
         self.align(center_table[0], center_table[1], np.deg2rad(1))
         self.setSpeed(100,0)
-        self.check_position_3_values(center_table[0], center_table[1], np.deg2rad(90), 20, np.Infinity, np.deg2rad(180))
+        self.check_position_3_values(center_table[0], center_table[1], np.deg2rad(90), 10, np.Infinity, np.deg2rad(180))
         #self.align()
         self.align(imgs_center[0], imgs_center[1], np.deg2rad(1))
        
