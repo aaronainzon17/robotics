307fd11cbb43c74e678b0e4360681f5a946d21e7
aaronainzon17 <aaronainzon@gmail.com>
2022-05-31 15:03:27 +0200

    print odometri as draw_robot

---
+++ Trabajo/drawRobot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+import sys
+import numpy as np
+from libraries.dibrobot import dibrobot
+
+if len(sys.argv) < 1:
+    print("Usage: drawRobot.py <file_name>") 
+    exit(1)
+
+file_name = str(sys.argv[1])
+f = open(file_name, "r")
+
+for x in f:
+    coords = x.split(",")
+    b = coords[2].split('\n')
+    a = np.array([float(coords[0]),float(coords[1]),float(b[0])])
+    dibrobot(a,'purple','s')
+plt.show()
