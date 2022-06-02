4d78a310ffce8042cb028daf111dda5413ac7bd4
aaronainzon17 <aaronainzon@gmail.com>
2022-06-02 11:59:20 +0200

    busqueda de bb8

--- Trabajo/ficheros_codigo_auxiliar/sample_matching.py
+++ Trabajo/ficheros_codigo_auxiliar/sample_matching.py
@@ -257,8 +257,8 @@ def main():
     found_BB8 = False
     
     #while not found_R2 and not found_BB8:
-    found_R2, x_R2 = find_template(mirror=mirror, img=None, refFilename='../R2-D2_s.png')
-        #found_BB8, x_BB8 = find_template(mirror=mirror, img=im, refFilename='../BB8_s.png')
+    #found_R2, x_R2 = find_template(mirror=mirror, img=None, refFilename='../R2-D2_s.png')
+    found_BB8, x_BB8 = find_template(mirror=mirror, img=im, refFilename='../BB8_s.png')
         #print(x_R2, x_BB8)
     #if x_R2 < x_BB8:
     #    print('R2-D2 esta a la izquierda')
