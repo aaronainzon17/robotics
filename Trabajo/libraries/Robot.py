e548a6b005397ad8829b8795db91372215c8c8b4
aaronainzon17 <aaronainzon@gmail.com>
2022-06-03 09:58:40 +0200

    cambio en el catch

--- Trabajo/libraries/Robot.py
+++ Trabajo/libraries/Robot.py
@@ -408,13 +408,13 @@ class Robot:
             
     # Funcion de captura de la pelota
     def catch(self):
-        w = 150 #40   # Velocidad angular para abrir las pinzas 
+        w = 300 #40   # Velocidad angular para abrir las pinzas 
         #Que avance un poquito antes de bajar la pinza
-        self.setSpeed(80,0)
-        time.sleep(1)
+        self.setSpeed(160,0)
+        time.sleep(0.5)
         self.setSpeed(0,0)
         self.BP.set_motor_dps(self.BP.PORT_A, w)
-        time.sleep(0.6) # Bajar cesta
+        time.sleep(0.25) # Bajar cesta
         self.BP.set_motor_dps(self.BP.PORT_A, 0)
         time.sleep(0.2)
         
